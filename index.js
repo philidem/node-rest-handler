beb31c98822cc5dd191237d8a3611762436f2c41
Phil Gates-Idem <phillip.idem@gmail.com>
2013-03-15 13:15:43 -0400

    Exposing RESTHandler in module

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ var RESTHandler = require('./lib/rest-handler/RESTHandler');
 module.exports = {
 	create : function(options) {
 		return new RESTHandler(options);
-	}
+	},
+
+	RESTHandler : RESTHandler
 };
